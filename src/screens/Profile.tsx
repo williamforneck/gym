6bac62c02c7796020a88edb9471c6f9282432a54
William Gabriel Forneck <62662711+williamforneck@users.noreply.github.com>
2023-09-14 21:03:37 -0300

    configurando o imagepicker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,3 +1,6 @@
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+
 import {
   Center,
   Heading,
@@ -5,6 +8,7 @@ import {
   Skeleton,
   Text,
   VStack,
+  useToast,
 } from "native-base";
 
 import { Button } from "@components/Button";
@@ -18,6 +22,46 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/williamforneck.png"
+  );
+
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  };
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -33,12 +77,12 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https://github.com/williamforneck.png" }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             ></UserPhoto>
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontFamily="heading"
